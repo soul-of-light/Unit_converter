6aa2fac5c09a4b016f27a51dc3526cc9e81b7af8
Daniel Humer <danielhumer.at@icloud.com>
2020-09-15 21:27:30 +0200

    small improvements

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-user = str(input("Hello my name is python and your name is? "))
+user = str(input("Hello, my name is Python and your name is? "))
 
 print("Hello " + user + ", in this program you can convert kilometers into miles.")
 
@@ -12,12 +12,14 @@ while True:
 
     miles = km * 0.621371
 
-    print("{0} kilometers is {1} miles.".format(km, miles))
+    print("{0} kilometers are {1} miles.".format(km, miles))
 
     choice = input("Would you like to do another conversion (y/n): ")
 
     if choice.lower() != "y" and choice.lower() != "yes":
         print("Goodbye " + user)
+        break
+
 
 
 
